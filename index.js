2349b8afda46d951195cd9cd96ef54f5f608c9fd
Laksmartt <lakshaymarwaha0104@gmail.com>
2025-04-10 18:53:47 -0400

    initialized the remaining routers and worked on css too

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const mainRouter = require("./routers/main.js");
 const bookRouter = require("./routers/books.js");
 const port = process.env.PORT || 3000;
 
+
 // Create & Configure App
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
+
+app.use(express.static("./public"));
 
 // Load routers
 app.use("", mainRouter);
--- routers/books.js
+++ routers/books.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const{
     getAllBooks,
-  
+    getBookById,
+    addBook
 } = require("../models/books.js");
 
 router.get("/", async (req, res) => {
@@ -13,5 +14,30 @@ router.get("/", async (req, res) => {
     }
 });
 
+
+router.get("/new", (req, res) => {
+    res.render("newBook", { title: "New Book" });
+});
+
+router.get("/:id", async (req, res) => {
+    const bookId = req.params.id;
+    const books = await getBookById(bookId);
+    if (books) {
+      res.render("book", { title: books.title, books });
+    } else {
+      res.redirect("/books");
+    }
+});
+
+router.post("/", async (req, res) => {
+    const author = req.body.author;
+    const title = req.body.title;
+    const isbn = req.body.isbn;
+  
+    const result = await addBook({ author, title, isbn});
+    const bookId = result.lastInsertRowid;
+  
+    res.redirect(`/books/${bookId}`);
+  });
 
 module.exports = router;
